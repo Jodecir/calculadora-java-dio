9b4f6a65e916f62cafb5593595b28a94b397e628
Jodecir Neto <jodecirneto@gmail.com>
2021-11-23 14:26:04 -0300

    CREATE repeating loop in calculator

--- src/course/com/dio/Calculadora.java
+++ src/course/com/dio/Calculadora.java
@@ -7,21 +7,31 @@ public class Calculadora {
         Scanner scan = new Scanner(System.in);
 
         int a, b;
+        boolean fechado = false;
+        String repetir;
 
-        System.out.println("Digite o primeiro valor: ");
-        a = scan.nextInt();
-        System.out.println("Digite o segundo valor: ");
-        b = scan.nextInt();
+        while (fechado == false) {
+            System.out.println("Digite o primeiro valor: ");
+            a = scan.nextInt();
+            System.out.println("Digite o segundo valor: ");
+            b = scan.nextInt();
 
-        int somar = somar(a,b);
-        int subtrair = subtrair(a,b);
-        int multiplicar = multiplicar(a,b);
-        int dividir = dividir(a,b);
+            int somar = somar(a,b);
+            int subtrair = subtrair(a,b);
+            int multiplicar = multiplicar(a,b);
+            int dividir = dividir(a,b);
 
-        System.out.println("soma: " + somar);
-        System.out.println("subtração: " + subtrair);
-        System.out.println("multiplicação: " + multiplicar);
-        System.out.println("divisão: " + dividir);
+            System.out.println("soma: " + somar);
+            System.out.println("subtração: " + subtrair);
+            System.out.println("multiplicação: " + multiplicar);
+            System.out.println("divisão: " + dividir);
+
+            System.out.println("Deseja continuar (s/n): ");
+            repetir = scan.next();
+            if (repetir == "n") {
+                fechado = true;
+            }
+        }
     }
 
     public static int somar(int a, int b) {
